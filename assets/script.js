58395852933fc5e855e102dbb4d3f5762158473b
Tristan Bond <tristanebond@gmail.com>
2020-11-11 21:31:21 -0800

    started maps api function

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,18 @@ var searchBtn = $()
 var cityEl = $()
 var mapEl = $()
 var listEl = $()
+
+
+//maps ajax call
+// api key =   AIzaSyA_zV314qmIaqz23j08ScL3Jo2NOk0ueKI
+{/* <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_zV314qmIaqz23j08ScL3Jo2NOk0ueKI&callback=initMap"
+  type="text/javascript"></script>  */}
+
+function createMaps(cityName){
+    $.ajax({
+        url:"",
+        method:"GET"
+    }).then({
+
+    });
+}

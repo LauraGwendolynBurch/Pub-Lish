fd6fbea62159be48dedec4cf38b1e6934345a399
Tristan Bond <tristanebond@gmail.com>
2020-11-13 19:14:06 -0800

    map and marker function

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,18 @@ var searchBtn = $()
 var cityEl = $()
 var mapEl = $()
 var listEl = $()
+var mapEl=$("#map");
 
 
-//maps ajax call
-// api key =   AIzaSyA_zV314qmIaqz23j08ScL3Jo2NOk0ueKI
-{/* <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_zV314qmIaqz23j08ScL3Jo2NOk0ueKI&callback=initMap"
-  type="text/javascript"></script>  */}
-
-function createMaps(cityName){
-    $.ajax({
-        url:"",
-        method:"GET"
-    }).then({
-
-    });
+  function createMarker(long, lat){
+    var marker = new mapboxgl.Marker()
+    .setLngLat([long,lat])
+    .addTo(map);
 }
+    
+    map.addControl(new mapboxgl.GeolocateControl({
+        positionOptions: {
+        enableHighAccuracy: true
+     },
+        trackUserLocation: true
+ }));

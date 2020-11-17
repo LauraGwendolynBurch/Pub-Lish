7b548956c0190751761d4a293dc2002c96ebec2f
Tristan Bond <tristanebond@gmail.com>
2020-11-16 20:33:19 -0800

    marker popup

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@ var cityEl;
 var pubAmount;
 var mapEl=$("#map");
 var marker;
+var NameOfCity;
+var typeOfBrew;
 
 // button listener for search button
 
@@ -10,6 +12,7 @@ searchBtn.on("click", function(event){
     event.preventDefault();
     cityEl = $("#cityName").val();
     pubAmount = $("#pubNumber").val();
+    $("#cityName").val("");
     buildQueryURL();
   // local storage
   
@@ -25,7 +28,8 @@ function buildQueryURL() {
       }).then(function(response){
         console.log(response);
         for(var i=0;i<pubAmount;i++){
-          console.log(response[i].longitude)
+          NameOfCity=response[i].name;
+          typeOfBrew=response[i].brewery_type;
           createMarker(response[i].longitude, response[i].latitude);
         }
       }); 
@@ -33,8 +37,35 @@ function buildQueryURL() {
 };
 
   function createMarker(long, lat){
+
+    // mapboxgl.accessToken = 'pk.eyJ1IjoidGJvbmQ0IiwiYSI6ImNraGVhOG51dDAwaDgydHBqbmNncnoxbngifQ.2PVL5DYBnVm1EeZOIUwTXw';
+    // marker= [long,lat];
+    // var map = new mapboxgl.Map({
+    // container: 'map',
+    // style: 'mapbox://styles/mapbox/light-v10',
+    // center: marker,
+    // zoom: 15
+    // });
+     
+    // // create the popup
+    var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+      NameOfCity + 
+      " Type of Brewery: " + typeOfBrew 
+    );
+     
+    // // create DOM element for the marker
+    // var el = document.createElement('div');
+    // el.id = 'marker';
+
+    // new mapboxgl.Marker(el)
+    // .setLngLat(marker)
+    // .setPopup(popup) // sets a popup on this marker
+    // .addTo(map1);
+    // console.log(map1);
+
      marker = new mapboxgl.Marker()
     .setLngLat([long,lat])
+    .setPopup(popup)
     .addTo(map);
     
 }
@@ -49,5 +80,6 @@ function buildQueryURL() {
  function removeMarker(marker){
   marker.remove();
  }
+ 
 
 
